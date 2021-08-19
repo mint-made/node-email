b9a75a7a6d4d3dc0b0d977845d56daefa3d4f795
mint-made <thomaskupai@gmail.com>
2021-08-19 17:27:16 +0100

    email form implemented as a less secure app

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,39 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 import express from 'express';
-
 const app = express();
+dotenv.config();
 
 // Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
-
 app.post('/api/email', (req, res) => {
-  res.send(req.body);
   console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.WORD,
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email Sent: ' + info.response);
+      res.send('Success');
+    }
+  });
 });
 
 const PORT = 5000;
